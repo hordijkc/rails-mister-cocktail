12be73fc2ca891963beb479192377c4a21b9d050
Chris Hordijk <hordijk.chris@gmail.com>
2019-05-17 11:35:48 +0200

    update dose

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,13 @@
+class IngredientsController < ApplicationController
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def show
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def create
+    @ingredient = Ingredient.new
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ingredient1 = Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "rum")
+Ingredient.create(name: "wodka")
+
+cocktail1 = Cocktail.create(name: "Bloody Mary")
+Cocktail.create(name: "Long Island Ice Tea")
+Cocktail.create(name: "White Russians")
+
+Dose.create(description: '30cl', cocktail: cocktail1, ingredient: ingredient1)
+# Dose.create(description: "1 cup")
+# Dose.create(description: "1 glass")
+
+# nested attributes for active records
