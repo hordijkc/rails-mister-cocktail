17b862b3c9df0b2a680bfe453b513e16c4d429a8
Chris Hordijk <hordijk.chris@gmail.com>
2019-05-20 16:28:50 +0200

    cocktail upgrade

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,14 @@
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import 'bootstrap';
+
+import { updateNavbar } from "../components/navbar"
+updateNavbar();
+
+import { loadDynamicBannerText } from '../components/banner';
+loadDynamicBannerText();
 
 console.log('Hello World from Webpacker')
-import 'bootstrap';
+
+
